a9df7bb52d94b58f1d960e7bba70b454db7306f8
zengrui <250476613@qq.com>
2023-09-29 19:33:37 +0800

    集成gin part 2

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"Hello": "world!",
+		})
+	})
+
+	r.NoRoute(func(ctx *gin.Context) {
+		acceptString := ctx.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			ctx.String(http.StatusNotFound, "页面返回404")
+		} else {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+	r.Run()
 }
