bb4e15222dc90eb630cdfe9a364acf59615d7673
zengrui <250476613@qq.com>
2023-10-01 16:06:48 +0800

    Create user model

---
+++ app/models/user/user_util.go
@@ -0,0 +1,17 @@
+package user
+
+import "mercury/pkg/database"
+
+// IsEmailExist check Email has been register
+func IsEmailExist(email string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("email = ?", email).Count(&count)
+	return count > 0
+}
+
+// IsPhoneExist check phone has been register
+func IsPhoneExist(phone string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
+	return count > 0
+}
