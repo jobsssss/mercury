97833e79495354f7a116d59c66ecf6d48e2bb389
zengrui <250476613@qq.com>
2023-10-03 15:49:36 +0800

    短信发送验证码接口

--- routes/api.go
+++ routes/api.go
@@ -9,16 +9,14 @@ import (
 func RegisterAPIRoutes(router *gin.Engine) {
 	v1 := router.Group("/v1")
 	{
-		authGroup := v1.Group("/auth")
-		{
-			signupCtl := new(ctlv1.SignupController)
-			authGroup.POST("/signup/phone/exist", signupCtl.IsPhoneExist)
-			authGroup.POST("/signup/email/exist", signupCtl.IsEmailExist)
+		signupCtl := new(ctlv1.SignupController)
+		v1.POST("/auth/signup/phone/exist", signupCtl.IsPhoneExist)
+		v1.POST("/auth/signup/email/exist", signupCtl.IsEmailExist)
 
-			// 发送验证码
-			vcc := new(ctlv1.VerifyCodeController)
-			// 图片验证码，需要加限流
-			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
-		}
+		// 发送验证码
+		vcc := new(ctlv1.VerifyCodeController)
+		// 图片验证码，需要加限流
+		v1.POST("/auth/verify-codes/captcha", vcc.ShowCaptcha)
+		v1.POST("/auth/verify-codes/phone", vcc.SendUsingPhone)
 	}
 }
