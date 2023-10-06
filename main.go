ea3d26a8deed0d9e44bbfe90f5446339ebcdba9f
zengrui <250476613@qq.com>
2023-10-06 17:51:41 +0800

    生成app key命令

--- main.go
+++ main.go
@@ -30,7 +30,10 @@ func main() {
 	}
 
 	// 注册子命令
-	rootCmd.AddCommand(cmd.CmdServe)
+	rootCmd.AddCommand(
+		cmd.CmdServe,
+		cmd.CmdKey,
+	)
 	// 配置默认运行 Web 服务
 	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
 	// 注册全局参数，--env
