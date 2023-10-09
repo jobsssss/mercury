d32f1a6b64264a45bb8de28800fb94a8c8060833
zengrui <250476613@qq.com>
2023-10-09 09:29:55 +0800

    缓存友情列表

--- app/controllers/v1/links_controller.go
+++ app/controllers/v1/links_controller.go
@@ -12,9 +12,6 @@ type LinksController struct {
 }
 
 func (ctrl *LinksController) Index(ctx *gin.Context) {
-	data, pager := link.Paginate(ctx, 10)
-	response.JSON(ctx, gin.H{
-		"data":  data,
-		"pager": pager,
-	})
+	links := link.AllCached()
+	response.Data(ctx, links)
 }
